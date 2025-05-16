a1e92cf82bab590f59914ddc3e9167bf93153d00
Dawson Eakins <dawsonm.eakins@gmail.com>
2025-05-16 13:02:05 -0400

    Among us background working as expected

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -4,7 +4,7 @@ import HistoryList from "../components/HistoryList";
 
 export default function History() {
   return (
-    <main className="min-h-screen bg-[#232220] text-[#ffddba] px-4">
+    <main className="min-h-screen  text-[#ffddba] px-4">
       <div className="max-w-4xl mx-auto pt-28 pb-24">
         <h1 className="text-4xl font-bold mb-8 text-center">🕘 Pick History</h1>
         <HistoryList />
